73c5bc5702e8904faef38c242554b442a039ba25
Rozhdestvenskaya <nvr20000@gmail.com>
2021-07-11 18:53:29 +0300

    Add decomposer.

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,35 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val squareNumber = number * number
+        val result = calculateTheSequence(squareNumber, number - 1)
+        return when {
+            result?.size == 1-> null
+            else -> result?.toTypedArray()
+        }
+    }
+
+    private fun calculateTheSequence(square: Int, num: Int): List<Int>? {
+        for (i in num downTo 1) {
+            val balance = square - i * i
+
+            if (balance == 0) return listOf(i)
+            else if (balance < 0) return null
+
+            val next = sqrt(balance.toDouble()).toInt()
+            var value: List<Int>? = emptyList()
+
+            if (next >= i) value = calculateTheSequence(balance, i - 1)
+            else value = calculateTheSequence(balance, next)
+
+                if (value != null) {
+                    return value + listOf(i)
+                }
+        }
+        return null
     }
 }
