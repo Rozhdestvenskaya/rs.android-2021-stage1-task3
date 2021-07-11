35a8c35ee1bd66e19e31fd75b904cee5e7b0d327
Rozhdestvenskaya <nvr20000@gmail.com>
2021-07-11 18:43:18 +0300

    Start app.

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,18 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var numberOfCombinations: Long = array[0].toLong() //6
+        val setOfElements = array[1] //4
+        var x = 0
+        if(setOfElements > 0 && array[0] >= 0){
+            for (i in  1..array[1]){
+                x = i
+                numberOfCombinations = factorial(setOfElements) / (factorial(x) * factorial(setOfElements - x))
+                if(numberOfCombinations == array[0].toLong()) return x
+            }
+        }
+        return null
     }
+
+    fun factorial(n: Int): Long = if (n < 2) 1 else n * factorial(n - 1)
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,31 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        var result = ArrayList<String>()
+        val plusOneArr = arrayOf(1, 2, 4, 5, 7, 8)
+        val minusOneArr = arrayOf(2, 3, 5, 6, 8, 9)
+        val plusThreeArr = arrayOf(1, 2, 3, 4, 5, 6)
+        val minusThreeArr = arrayOf(4, 5, 6, 7, 8, 9)
+        val minusEightArr = arrayOf(8)
+        val plusEightArr = arrayOf(0)
+        val regex = "[0-9]+".toRegex()
+        if(number.matches(regex) && number.toInt() >= 0){
+            for (i in number.indices){
+                if(plusOneArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]+1).toString() + number.substring(i+1))
+                if(minusOneArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]-1).toString() + number.substring(i+1))
+                if(minusThreeArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]-3).toString() + number.substring(i+1))
+                if(plusThreeArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]+3).toString() + number.substring(i+1))
+                if(plusEightArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]+8).toString() + number.substring(i+1))
+                if(minusEightArr.contains(number[i].toString().toInt()))
+                    result?.add(number.substring(0, i) + (number[i]-8).toString() + number.substring(i+1))
+            }
+            return result?.toTypedArray()
+        }
+        return null
     }
 }
